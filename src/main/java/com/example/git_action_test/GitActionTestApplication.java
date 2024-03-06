d223c498b1ad11500e6c4fac4a6279ef7fe21234
preferrrr <zzangsh2946@naver.com>
2024-03-06 13:20:34 +0900

    test

--- src/main/java/com/example/git_action_test/GitActionTestApplication.java
+++ src/main/java/com/example/git_action_test/GitActionTestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitActionTestApplication {
 
     public static void main(String[] args) {
+        //test
         SpringApplication.run(GitActionTestApplication.class, args);
     }
 
