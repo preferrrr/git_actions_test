848700f217a4204f77938fbda116a3d197ccf5cd
preferrrr <zzangsh2946@naver.com>
2024-03-06 11:26:50 +0900

    first commit

---
+++ src/main/java/com/example/git_action_test/GitActionTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_action_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitActionTestApplication.class, args);
+    }
+
+}
